973003b62e303b34562235142d154db5b531ea17
Szymon Luniewski <sizmon15@gmail.com>
2019-05-18 12:57:07 +0100

    metoda naiwna

---
+++ src/main/java/com/sda/sito/erastotenesa/App.java
@@ -0,0 +1,28 @@
+package com.sda.sito.erastotenesa;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int k = 2;
+        while (k < 200) ;
+        {
+            int licznik = 0;
+
+
+            for (int i = 2; i < k; i++) {
+                if (k % i == 0) {
+                    licznik++;
+                }
+            }
+            if (licznik == 0) {
+                System.out.println(k + "jest liczba pierwsza");
+            } else {
+                System.out.println(k + "nie jest liczba pierwsza");
+            }
+            k++;
+        }
+
+    }
+}
